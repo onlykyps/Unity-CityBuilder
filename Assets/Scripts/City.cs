1c08e97d058f43cbd519f6fd8fb1a82b8ff8bbfa
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2024-08-24 13:39:04 +0300

    notes on city resource manager
    and other modifications

--- Assets/Scripts/City.cs
+++ Assets/Scripts/City.cs
@@ -2,6 +2,19 @@ using UnityEngine;
 
 public class City : MonoBehaviour
 {
+    // notes on city resource manager
+
+    // jobsCeiling = factories * 10
+    // currentJobs = min(currentPopulation, jobsCeiling)
+    // currentFood += farms*4
+
+    // populationCeiling = houses*5
+    // if currentFood >= currentPopulation && currentPopulation < populationCeiling
+        // currentFood -= currentPopulation * .25f
+        // currentPopulation = min(currentPopulation += currentFood * .20f, populationCeiling)
+
+    // cash += currentJobs * 2
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
