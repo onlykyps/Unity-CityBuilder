e42646fd5e587d5d6deed4454409adee22b4408a
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2024-08-24 12:37:57 +0300

    Camera controller
    monobehaviour script

---
+++ Assets/Scripts/CameraCTRL.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CameraCTRL : MonoBehaviour
+{
+    
+     private void LateUpdate()
+    {
+        Camera.main.orthographicSize = 10;
+    }
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
