6e3882b4b12934ccf5ed049802d7e4907b7d4cb9
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2024-08-24 14:30:19 +0300

    city resource manager

--- Assets/Scripts/City.cs
+++ Assets/Scripts/City.cs
@@ -15,10 +15,66 @@ public class City : MonoBehaviour
 
     // cash += currentJobs * 2
 
+    public int Cash {get; set;}
+    public int Day {get; set;}
+    public float PopulationCurrent {get; set;}
+    public float PopulationCeiling {get; set;}
+    public int JobsCurrent {get; set;}
+    public int JobsCeiling {get; set;}
+    public float Food {get; set;}
+
+    public int[] buildingCounts = new int[3];
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cash = 100000;
+        Food = 6;
+        JobsCeiling = 10;
+    }
+
+    public void EndTurn()
+    {
+        Day++;
+        CalculatePopulation();
+        CalculateJob();
+        CalculateFood();
+        CalculateCash();
+        Debug.Log("Day ended.");
+        Debug.LogFormat
+        ("Jobs: {0}/{1}, Cash:{2}, Pop:{3}/{4}, Food{5}",
+        JobsCurrent, JobsCeiling, Cash, PopulationCurrent, PopulationCeiling, Food);
+    }
+
+    void CalculateJob()
+    {
+        JobsCeiling = buildingCounts[2] * 10;
+        JobsCurrent = Mathf.Min((int)PopulationCurrent, JobsCeiling);
+    }
+
+    void CalculateCash()
+    {
+        Cash +=JobsCurrent * 2;
+    }
+
+    void CalculateFood()
+    {
+        Food += buildingCounts[1] * 4f;
+    }
+
+    void CalculatePopulation()
+    {
+        PopulationCeiling = buildingCounts[0] * 5;
+        if(Food >= PopulationCurrent && PopulationCurrent < PopulationCeiling)
+        {
+            Food -= PopulationCurrent * .25f;
+            PopulationCurrent = Mathf.Min(PopulationCurrent += Food * .25f, PopulationCeiling);
+        }
+        else if (Food < PopulationCurrent)
+        {
+            PopulationCurrent -= (PopulationCurrent - Food) * .5f;
+
+        }
     }
 
     // Update is called once per frame
