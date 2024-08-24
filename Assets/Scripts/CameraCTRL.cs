98b1a544f13bd8dda2e28f6fbf8a70f9aa15ae8d
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2024-08-24 13:04:57 +0300

    camera ctrl
    zoom in/out
    pan by dragging

--- Assets/Scripts/CameraCTRL.cs
+++ Assets/Scripts/CameraCTRL.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class CameraCTRL : MonoBehaviour
 {
+    private Vector3 cameraOriginPoint;
+    private Vector3 offset;
+    private bool dragging;
     
      private void LateUpdate()
     {
-        Camera.main.orthographicSize = 10;
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * 
+        (10f * Camera.main.orthographicSize * .1f), 2.5f, 50f) ;
+
+        if (Input.GetMouseButton(2))
+        {
+            offset = ();
+        }
     }
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
